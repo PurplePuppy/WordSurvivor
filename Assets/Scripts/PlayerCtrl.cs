c30c442886a82dcc257c1e462f2303deda867fd8
PurplePuppy <dlgowns2443@naver.com>
2026-02-01 22:42:22 +0900

    몬스터 충돌, 아이템 획득
    몬스터 충돌 시 체력 감소, 아이템 획득 시 아이템 제거 및 임시로 경험치 증가

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -3,37 +3,64 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    private Rigidbody2D rigid;
     private bool pause;
 
     [HideInInspector]
     public float speed;
+    public float hp;
+    public float exp;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
 
     void Start()
     {
         pause = false;
         speed = 3.0f;
+        hp = 100.0f;
+        exp = 0.0f;
     }
 
     void Update()
     {
         if(pause)
             return;
+        
+        if(hp <= 0.0f)
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
 
-        if(Input.GetKey(KeyCode.W))
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(moveX, moveY, 0);
+        
+        if (moveDirection.magnitude > 1f)
         {
-            transform.Translate(Vector3.up * speed * Time.deltaTime);
+            moveDirection.Normalize();
         }
-        if(Input.GetKey(KeyCode.S))
+        rigid.linearVelocity = transform.TransformDirection(moveDirection) * speed;
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
         {
-            transform.Translate(Vector3.down * speed * Time.deltaTime);
+            hp -= 0.1f;
         }
-        if(Input.GetKey(KeyCode.A))
+
+        if(collision.gameObject.tag == "Item")
         {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            exp += 1.0f;
+            Destroy(collision.gameObject);
+            Debug.Log("Item Get!");
         }
     }
 }
