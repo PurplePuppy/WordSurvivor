3346ac5cb4db03c10fefb6dd459b6d9525255046
enirobot <enirobot@naver.com>
2026-02-02 17:25:31 +0900

    feat(title): 타이틀 씬에 시작/종료 버튼 추가 및 SceneLoader에 Quit 기능 추가
    타이틀 씬에 Start/Quit 버튼 추가 및 UI 조정(1920x1080). SceneLoader에 QuitGame() 메서드 추가하여 에디터/빌드 환경에서 안전하게 종료하도록 구현.
    
    변경파일:
    - Assets/Title.unity
    - Assets/Scenes/SceneLoader.cs

--- Assets/Scenes/SceneLoader.cs
+++ Assets/Scenes/SceneLoader.cs
@@ -9,4 +9,13 @@ public class SceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene(gameSceneName);
     }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
 }
